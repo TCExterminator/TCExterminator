5a358c717d614b12ce936aa1a1860a1d6b6a6f88
Bertrand JAMAIN <jamai002@pc142.iut-rcc-info.urca>
2015-06-16 12:21:39 +0200

    premier commit

---
+++ Jeu.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Jeu {
+	private ArrayList<Terrain> lesTerrains;
+	private ArrayList<Entite> lesEntites;
+	
+	//méthode principal
+	public void main(){
+		//à coder
+	}
+	
+	//méthode servant à afficher le jeu
+	public void afficher(){
+		// à coder
+	}
+	
+	//verifieSuppression()
+	
+	
+}
