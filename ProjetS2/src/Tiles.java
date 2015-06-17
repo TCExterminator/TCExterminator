90415fd8892a58e03eba67e629f102b354e60b21
TCExterminator <john.doe@example.com>
2015-06-17 11:22:10 +0200

    ber

---
+++ ProjetS2/src/Tiles.java
@@ -0,0 +1,14 @@
+import org.newdawn.slick.opengl.Texture;
+public class Tiles {
+	private char typeCase;
+	private Texture text;
+	
+	public char getTypeCase(){
+		return this.typeCase;
+	}
+	
+	public void setTypeCase(char tyC){
+		this.typeCase = tyC;
+	}
+	
+}
