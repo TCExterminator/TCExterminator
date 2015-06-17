fc4b49678b6c669e2c0c781aa0d91a627cf4c799
TCExterminator <bertrand.jamain@etudiant.univ-reims.fr>
2015-06-17 10:58:20 +0200

    alexis

--- ProjetS2/src/Projectile.java
+++ ProjetS2/src/Projectile.java
@@ -2,7 +2,7 @@
 public class Projectile extends Entity{
 	private int puissanceProjectile;
 	
-	public Projectile(int x, int y, int speed, String texture, int puissanceProjectile){
+	public Projectile(int dX, int dY, int x, int y, int speed, String texture, int puissanceProjectile){
 		super(x, y, speed, texture);
 		this.puissanceProjectile = puissanceProjectile;
 	}
