fbe3e3bb4a5b0c50155f2adf2496eb278da2e439
Shinkagi <tonyhun.s@gmail.com>
2015-06-18 16:49:52 +0200

    Ninzaaaa

--- ProjetS2/src/Player.java
+++ ProjetS2/src/Player.java
@@ -13,7 +13,7 @@ public class Player
   
   public Player(int posx, int posy, int speed)
   {
-    super(posx, posy, speed, "perso", 15, 500);
+    super(posx, posy, speed, "ninja", 15, 500);
     this.inventaire.add(Jeu.listeArme[1]);
   }
   
