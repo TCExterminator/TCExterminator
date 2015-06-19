9d39a2c3b6bdd7031e5fc5a6f7f5db95f0fb7bc9
Shinkagi <tonyhun.s@gmail.com>
2015-06-19 12:21:59 +0200

    Javadoc Projectile

--- ProjetS2/src/Projectile.java
+++ ProjetS2/src/Projectile.java
@@ -6,6 +6,16 @@ public class Projectile
   private double DY;
   private double DX;
   
+  /**
+   * Constructeur parametré de Projectile
+   * @param dx vecteur x
+   * @param dy vecteur y
+   * @param x position
+   * @param y position
+   * @param speed 
+   * @param puissanceProjectile
+   * @param porte
+   */
   public Projectile(int dx, int dy, int x, int y, int speed, int puissanceProjectile,int porte)
   {
     super(x, y, speed, "bullet", 2, porte);
@@ -16,6 +26,18 @@ public class Projectile
     this.DX = (dx / this.coefDir);
   }  
   
+  /**
+   * Constructeur parametré de Projectile
+   * Une texture en plus est demandée pour les projectiles personnalisés (couteau)
+   * @param dx
+   * @param dy
+   * @param x
+   * @param y
+   * @param speed
+   * @param puissanceProjectile
+   * @param porte
+   * @param texture
+   */
   public Projectile(int dx, int dy, int x, int y, int speed, int puissanceProjectile,int porte,String texture)
   {
     super(x, y, speed, texture, 1, porte);
@@ -26,20 +48,38 @@ public class Projectile
     this.DX = (dx / this.coefDir);
   }
   
+  /**
+   * getter de DY
+   * @return
+   */
   public double getDY()
   {
     return this.DY;
   }
   
+  /**
+   * getter de DX
+   * @return
+   */
   public double getDX()
   {
     return this.DX;
   }
+  
+  /**
+   * méthode qui décrémente la variable sante
+   * @param d
+   */
   public void heatSante(int d){
 	  this.sante-=d;
   }
   
-  public int getDegat(){
+  /**
+   * getter de puissanceProjectile
+   * appelé getDegat comme puissanceProjectile est un nom à rallonge (merci Alexis).
+   * @return
+   */
+  int getDegat(){
 	  return this.puissanceProjectile;
   }
   
