0296340672450d2257a9c5c2040054fa4690bbb8
Shinkagi <tonyhun.s@gmail.com>
2015-06-19 12:43:44 +0200

    ZOMBUNNY

--- ProjetS2/src/Zombie.java
+++ ProjetS2/src/Zombie.java
@@ -6,7 +6,7 @@ public class Zombie
   //constructeur parametre de la clase Zombie : la position ( int, int ) et la vitesse (int) sont en parametres
   public Zombie(int x, int y, int speed)
   {
-    super(x, y, speed, "TCboy", 15, 20);
+    super(x, y, speed, "Zombbunny", 15, 20);
     this.degat *= 1;
   }
   
