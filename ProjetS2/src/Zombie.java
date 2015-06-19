a3fa73d69693442d31fe0f39f90575d46f3928fa
TCExterminator <bertrand.jamain@etudiant.univ-reims.fr>
2015-06-19 10:35:33 +0200

    alexis

--- ProjetS2/src/Zombie.java
+++ ProjetS2/src/Zombie.java
@@ -3,19 +3,14 @@ public class Zombie
 {
   private int degat = 20;
   
+  //constructeur parametre de la clase Zombie : la position ( int, int ) et la vitesse (int) sont en parametres
   public Zombie(int x, int y, int speed)
   {
     super(x, y, speed, "TCboy", 15, 20);
     this.degat *= 1;
   }
   
-  public boolean collision(Entity entite)
-  {
-    boolean res = false;
-    //HI BIATCH
-    return res;
-  }
-  
+  // getter des degats du zombie
   public int getDegat(){
 	  return this.degat;
   }
