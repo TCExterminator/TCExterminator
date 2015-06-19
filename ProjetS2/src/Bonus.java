bc66163fde8963bf49f860cbd88917e2dd15ffaa
TCExterminator <bertrand.jamain@etudiant.univ-reims.fr>
2015-06-19 10:22:14 +0200

    alexis

--- ProjetS2/src/Bonus.java
+++ ProjetS2/src/Bonus.java
@@ -11,6 +11,7 @@ public class Bonus
   private Arme a = null;
   private Texture textureBonus;
   
+  // constructeur parametre de la classe Bonus : la position ( int, int) et l'effet ( int) sont en parametres
   public Bonus(int x, int y, int effet)
   {
     this.textureBonus = Jeu.getTexture("pika");
@@ -19,6 +20,7 @@ public class Bonus
     this.effet = effet;
   }
   
+  // constructeur parametre de la classe Bonus : la position ( int, int ) est en prametre
   public Bonus(int x, int y)
   {
     this.textureBonus = Jeu.getTexture("pika");
@@ -27,6 +29,7 @@ public class Bonus
     this.effet = 5;
   }
   
+  // méthode servant à afficher un Bonus selon une texture
   public void afficher()
   {
     this.textureBonus.bind();
@@ -34,41 +37,50 @@ public class Bonus
     GL11.glEnd();
   }
   
+  // getter de la position en X
   public int getPosX()
   {
     return this.posX;
   }
   
+  // getter de la position en Y
   public int getPosY()
   {
     return this.posY;
   }
   
+  // getter de l'effet
   public int getEffet()
   {
     return this.effet;
   }
   
+  // getter de l'Arme
   public Arme getArme()
   {
     return this.a;
   }
   
+  
+  // setter de la position en X
   public void setPosX(int x)
   {
     this.posX = x;
   }
   
+  //setter de la position en Y
   public void setPosY(int y)
   {
     this.posY = y;
   }
   
+  // setter de l'effet
   public void setEffet(int effet)
   {
     this.effet = effet;
   }
   
+  // setter de l'Arme
   public void setArme(Arme arm)
   {
     this.a = new Arme(arm);
