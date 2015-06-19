7fc76571e545fdc9826abaf1a8bebed0dc5d2966
Shinkagi <tonyhun.s@gmail.com>
2015-06-19 12:23:54 +0200

    Javadoc Tiles

--- ProjetS2/src/Tiles.java
+++ ProjetS2/src/Tiles.java
@@ -5,28 +5,49 @@ public class Tiles
   private char typeTiles;
   private Texture textur;
   
+  /**
+   * Constructeur parametré de Tiles
+   * @param tex
+   * @param typeTiles
+   */
   public Tiles(Texture tex, char typeTiles)
   {
     this.textur = tex;
     this.typeTiles = typeTiles;
   }
   
+  /**
+   * getter de typeTiles
+   * @return
+   */
   public char getTypeTiles()
   {
     return this.typeTiles;
   }
   
+  /**
+   * setter du typeTiles
+   * @param tyC
+   */
   public void setTypeTiles(char tyC)
   {
 	  
     this.typeTiles = tyC;
   }
   
+  /**
+   * setter de textur
+   * @param textur
+   */
   public void setTexture(Texture textur)
   {
     this.textur = textur;
   }
   
+  /**
+   * getter de textur
+   * @return
+   */
   public Texture getTexture()
   {
     return this.textur;
